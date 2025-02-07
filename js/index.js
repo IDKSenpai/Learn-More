2683e8e6aab868c164a0120ce3fa303779201f9b
Kech Kheang <k.kheang087@gmail.com>
2025-02-07 20:46:36 +0700

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const validateLogin = () => {
+const validateLogin = (event) => {
+  event.preventDefault();
+
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
